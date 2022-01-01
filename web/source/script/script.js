b8c5df6630c921f96623388d08def67eb7177d58
Michal <michaldacko4@wp.pl>
2022-01-02 00:42:40 +0100

    Adding some config files and structure folders

---
+++ web/source/script/script.js
@@ -0,0 +1,6 @@
+import _ from 'lodash-core';
+import * as dateFns from "date-fns";
+console.log((dateFns.isDate(213)));
+const myArray = [1, 2, 3, 4];
+console.log(_.chunk(myArray, 2));
+console.log("cos");
